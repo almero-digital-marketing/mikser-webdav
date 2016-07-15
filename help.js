72214eb2730e7c9eabcb29e928466fe2bd30e6a6
Dimitar Kolev <dick@ingbg.com>
2016-07-15 20:00:15 +0300

    Fixed regression

--- help.js
+++ help.js
@@ -1,5 +1,6 @@
 'use strict';
 var jsDAV_ServerPlugin = require("jsDAV/lib/DAV/plugin");
+var jsDAV_iFile = require("jsDAV/lib/DAV/interfaces/iFile");
 var path = require('path');
 var fs = require("fs");
 
@@ -14,12 +15,17 @@ var jsDAV_Help_Plugin = module.exports = jsDAV_ServerPlugin.extend({
     httpGetInterceptor: function(e, method) {
         if (method != "GET") return e.next();
         var self = this;
-		self.handler.httpResponse.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
-		fs.readFile(path.join(__dirname, 'help.html'), (err, data) => {
-			if (err) return console.log(err);
-			self.handler.httpResponse.end(data);
-			e.stop();
-		});
+        var uri = this.handler.getRequestUri();
+        this.handler.getNodeForPath(uri, (err, node) => {
+			if (err || node.hasFeature(jsDAV_iFile)) return e.next();
+
+			self.handler.httpResponse.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
+			fs.readFile(path.join(__dirname, 'help.html'), (err, data) => {
+				if (err) return console.log(err);
+				self.handler.httpResponse.end(data);
+				e.stop();
+			});
+        });
     },
 
 });
